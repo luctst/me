6c6b25c12ad37d0d11715711a38d2e50f7bda983
luctst <lucas.tostee@gmail.com>
2022-02-28 13:49:57 +0100

    Init ✨

---
+++ components/Loader.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="container loader"></section>
+</template>
+
+<script>
+export default {
+  name: 'loader',
+  data() {
+    return {
+      loading: false,
+    };
+  },
+  methods: {
+    start() {
+      this.loading = true;
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.loader {
+  background: #E5E5E5;
+  height: 100vh;
+  padding: 0 15px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="container">
+    <h1></h1>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+main {
+  background: red;
+
+  h1 {
+    font-size: 21px;
+  }
+}
+</style>
