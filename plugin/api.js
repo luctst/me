115db3936f03c7a8e79d27dba41faf96637537eb
luctst <lucas.tostee@gmail.com>
2022-03-02 07:04:42 +0100

    Update route api for dev

--- plugin/api.js
+++ plugin/api.js
@@ -2,7 +2,7 @@ export default function ({ $axios }, inject) {
   // Create a custom axios instance
   const api = $axios.create();
   api.setHeader('Authorization', `bearer ${process.env.STRAPIKEY}`);
-  api.setBaseURL(process.env.API_URL_DEV);
+  api.setBaseURL(process.env.NODE_ENV === 'development' ? process.env.API_URL_DEV : '');
   // Inject to context as $api
   inject('api', api)
 }
