aacbcea85b3c59587ff745851a14cd633850d70b
luctst <lucas.tostee@gmail.com>
2022-09-28 23:12:41 +0200

    ci: update nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/google-analytics',
-    ['@nuxtjs/dotenv', { path: './' }],
+    '@nuxtjs/dotenv',
     "@nuxtjs/svg",
     '@nuxtjs/eslint-module',
   ],
