7ba31ea093bcb25a4912c28480ee10b29bd3bbd5
Lucas Tostee <lucas.tostee@gmail.com>
2025-06-07 01:01:50 +0200

    chore: remove legacy files

--- plugin/api.js
+++
@@ -1,6 +0,0 @@
-export default function ({ $axios }, inject) {
-  const api = $axios.create();
-  // api.setHeader('Authorization', `Bearer ${process.env.STRAPIKEY}`);
-  api.setBaseURL(`${process.env.NUXT_ENV_API_URL}?populate=%2A&sort=createdAt:asc`);
-  inject('api', api)
-}
